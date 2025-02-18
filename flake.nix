866c801f657902be3b8a3416263958ebd22aa402
Pablo Martínez <pablomartan@kolabnow.com>
2025-02-18 22:49:31 +0100

    feat: export emacs package with custom configuration

--- flake.nix
+++ flake.nix
@@ -3,11 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nix-emacs = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {nixpkgs, ...}: let
-    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  outputs = {
+    nixpkgs,
+    nix-emacs,
+    ...
+  }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [
+        nix-emacs.overlays.default
+      ];
+    };
   in {
+    packages."x86_64-linux".default = pkgs.emacsWithPackagesFromUsePackage {
+      package = pkgs.emacs.override {withGTK3 = true;};
+      config = ./init.el;
+      alwaysEnsure = true;
+      defaultInitFile = true;
+    };
     devShells."x86_64-linux".default = pkgs.mkShell {
       buildInputs = with pkgs; [
         emacs
