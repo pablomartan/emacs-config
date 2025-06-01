0e8997cdb5246827adf9f5e9523aaf05f6d6c887
Pablo Martínez <pablomartan@kolabnow.com>
2025-06-01 20:55:51 +0200

    feat: update flake to latest stable nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My emacs exploration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nix-emacs = {
       url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
