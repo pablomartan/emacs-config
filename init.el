6892c568a921295cf09237e8ef2fe7bb1d1746c7
Pablo Martínez <pablomartan@kolabnow.com>
2025-06-08 08:44:16 +0200

    feat(evil): add open at point evil keybinding

--- init.el
+++ init.el
@@ -132,6 +132,7 @@
     (evil-define-key 'normal 'global (kbd "<leader>oc") 'org-capture)
     (evil-define-key 'normal 'global (kbd "<leader>oa") 'org-agenda)
     (evil-define-key 'normal 'global (kbd "<leader>ol") 'org-store-link)
+    (evil-define-key 'normal 'global (kbd "<leader>oo") 'org-open-at-point)
     (evil-define-key 'normal 'global (kbd "<leader>j") 'jump-to-register)
     (evil-define-key 'normal 'global (kbd "<leader>u") 'undo-tree-visualize)
     (evil-define-key 'normal org-mode-map
