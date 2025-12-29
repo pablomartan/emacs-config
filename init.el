27ce22801cf5733868546c5ec57c7246d5750bdd
Pablo Martínez <pablomartan@kolabnow.com>
2025-12-29 20:48:24 +0100

    feat: add spacing and visible margin for windows

--- init.el
+++ init.el
@@ -72,3 +72,26 @@
       '(("agenda.org" . (:level . 1))
 	("projects.org" . (:level . 1))
 	("someday.org" . (:maxlevel . 3))))
+
+(require 'frame)
+
+;; Default frame settings
+(setq default-frame-alist '((min-height . 1)  '(height . 45)
+                            (min-width  . 1)  '(width  . 81)
+                            (vertical-scroll-bars . nil)
+                            (internal-border-width . 16)
+                            (left-fringe . 0)
+                            (right-fringe . 0)
+                            (tool-bar-lines . 0)
+                            (menu-bar-lines . 1)))
+
+;; Default frame settings
+(setq initial-frame-alist default-frame-alist)
+
+(setq-default window-divider-default-right-width 2
+              window-divider-default-places 'right-only
+              left-margin-width 0
+              right-margin-width 0)
+              ; window-combination-resize nil) ; Do not resize windows proportionally
+
+(window-divider-mode 1)
